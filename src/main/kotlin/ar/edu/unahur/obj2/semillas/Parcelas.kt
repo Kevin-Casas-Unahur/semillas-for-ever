a32fe9f88cc4afc6aee964a4900fde7fd1e15e2b
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2022-04-09 17:05:07 -0300

    config funcion superficie de clase Parcelas

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
@@ -1,3 +1,9 @@
 package ar.edu.unahur.obj2.semillas
 
-class Parcelas() {}
+class Parcelas(val ancho : Double, val largo : Double, val horasDeSol : Int) {
+    var plantas = mutableListOf<Planta>()
+
+    fun superficie() = ancho * largo
+
+    fun cantMaximaDePlantas() = if(ancho > largo) {}
+}

f8dfc3ba0d4025f50696d7554a673c04116116f2
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2022-04-09 17:14:18 -0300

    config funcion plantar de clase Parcelas

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcelas.kt
@@ -8,4 +8,6 @@ class Parcelas(val ancho : Double, val largo : Double, val horasDeSol : Int) {
     fun cantMaximaDePlantas() = if(ancho > largo) {this.superficie() / 5} else {(this.superficie() / 3) + largo}
 
     fun tieneComplicaciones() = plantas.any { it.horasDeSolToleradas() < horasDeSol }
+
+    fun plantar(semilla : Planta) {plantas.add(semilla)}
 }

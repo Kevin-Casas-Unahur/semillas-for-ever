49df3c69b931a78e79378894616fd1e768f55fd7
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2022-04-19 19:39:02 -0300

    Agregado collecion parcelas a objeto Inta

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Inta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Inta.kt
@@ -1,5 +1,9 @@
 package ar.edu.unahur.obj2.semillas
 
 object Inta {
+    val parcelas = mutableListOf<Parcelas>()
 
+    fun promedioPlantasPorParcela() : Any {
+
+    }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/semillas/IntaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/IntaTest.kt
@@ -1,9 +1,13 @@
 package ar.edu.unahur.obj2.semillas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class IntaTest : DescribeSpec ({
-    describe("Set de estadísticas del instituto INTA") {
+    describe("Set de estadísticas del instituto Inta") {
 
+        it("El promedio de 0 plantas") {
+            Inta.promedioPlantasPorParcela().shouldBe(0)
+        }
     }
 })

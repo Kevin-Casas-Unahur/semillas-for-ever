a6fbf47f2eefb60cc4785c34cf5c7d0e8817dfc4
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2022-04-19 19:32:27 -0300

    Inicializado objeto Inta

---
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/IntaTest.kt
@@ -0,0 +1,9 @@
+package ar.edu.unahur.obj2.semillas
+
+import io.kotest.core.spec.style.DescribeSpec
+
+class IntaTest : DescribeSpec ({
+    describe("Set de estadísticas del instituto INTA") {
+
+    }
+})

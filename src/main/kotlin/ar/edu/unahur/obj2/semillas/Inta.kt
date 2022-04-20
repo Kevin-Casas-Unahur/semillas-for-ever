22e37d5e7ee4e2446e9c33a493f996db644e2e12
Kevin-Casas-Unahur <kevinaxel.casas@estudiantes.unahur.edu.ar>
2022-04-19 21:00:56 -0300

    Agregado tests de promedio de plantas de el objeto Inta

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Inta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Inta.kt
@@ -3,7 +3,12 @@ package ar.edu.unahur.obj2.semillas
 object Inta {
     val parcelas = mutableListOf<Parcelas>()
 
-    fun promedioPlantasPorParcela() : Any {
-
+    //Retorna el promedio de plantas por parcela
+    //Número de plantas en todas las parcelas divididas el número de parcelas
+    //Si no hay ninguna parcela retorna 0
+    fun promedioPlantasPorParcela() : Int {
+        val cantPlantas = parcelas.sumOf { it.plantas.size }
+        val cantParcelas = parcelas.size
+        return if (cantParcelas == 0) {0} else {cantPlantas / cantParcelas}
     }
 }
--- src/test/kotlin/ar/edu/unahur/obj2/semillas/IntaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillas/IntaTest.kt
@@ -4,10 +4,53 @@ import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class IntaTest : DescribeSpec ({
-    describe("Set de estadísticas del instituto Inta") {
 
-        it("El promedio de 0 plantas") {
+    describe("Promedio por parcelas") {
+
+        it("El promedio de 0 parcelas") {
             Inta.promedioPlantasPorParcela().shouldBe(0)
+        }
+
+        it("El promedio de 1 sola parcela es igual a su numero de plantas") {
+            val parcelaIndus1 = ParcelaIndustrial(10.0, 10.0, 10)
+            val peperina1 = Peperina(0.5, 2021)
+            parcelaIndus1.plantar(peperina1)
+            Inta.parcelas.add((parcelaIndus1))
+            Inta.promedioPlantasPorParcela().shouldBe(parcelaIndus1.plantas.size)
+        }
+
+        it("El promedio de 3 sola parcelas con 3, 2 y 1 plantas respectivamente") {
+            Inta.parcelas.clear()
+
+            val parcelaIndus1 = ParcelaIndustrial(10.0, 10.0, 10)
+            val parcelaIndus2 = ParcelaIndustrial(10.0, 10.0, 10)
+            val parcelaEco1 = ParcelaEcologica(10.0, 10.0, 10)
+
+            val peperina1 = Peperina(0.5, 2021)
+            val peperina2 = Peperina(0.3, 2021)
+            val peperina3 = Peperina(0.7, 2021)
+
+            parcelaIndus1.plantar(peperina1)
+
+            parcelaIndus2.plantar(peperina1)
+            parcelaIndus2.plantar(peperina2)
+
+            parcelaEco1.plantar(peperina1)
+            parcelaEco1.plantar(peperina2)
+            parcelaEco1.plantar(peperina3)
+
+            Inta.parcelas.add((parcelaIndus1))
+            Inta.parcelas.add((parcelaIndus2))
+            Inta.parcelas.add((parcelaEco1))
+
+            Inta.promedioPlantasPorParcela().shouldBe(2)
+        }
+    }
+    describe("Parcela mas autosustentable") {
+        Inta.parcelas.clear()
+
+        it("Si no hay ninguna debe avisar") {
+            
         }
     }
 })
